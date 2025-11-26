809f6b688656d8a59cd60f0341d0355bc0c9e954
Aliaksandr <am@mediacube.io>
2025-11-26 09:05:35 +0700

    feat: add tracks logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { UsersController } from './controllers/user.controller';
 import { UsersService } from './services/user.services'
-import { AppService } from './app.service';
+import { TrackController } from './controllers/track.controller';
+import { TrackService } from './services/track.services';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [UsersController, TrackController],
+  providers: [UsersService, TrackService],
 })
 export class AppModule {}
---
+++ src/types/track.types.ts
@@ -0,0 +1,14 @@
+export interface Track {
+  id: string; // uuid v4
+  name: string;
+  artistId: string | null; // refers to Artist
+  albumId: string | null; // refers to Album
+  duration: number; // integer number
+}
+
+export type TrackDto = {
+  name: string;
+  artistId: string | null; // refers to Artist
+  albumId: string | null; // refers to Album
+  duration: number; // integer number
+}
